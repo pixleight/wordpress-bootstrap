67bd9b350335ed78b2bf8829ec29ca8004b9b8c7
Chris Violette <cvioletterun@gmail.com>
2014-02-20 14:52:05 -0500

    Updated department archive pages

---
+++ archive-department.php
@@ -0,0 +1,77 @@
+<?php get_header(); ?>
+			
+			<div id="content" class="clearfix row">
+			
+				<div id="main" class="col-sm-6 clearfix" role="main">
+				
+					<div class="page-header">
+						<h1 class="archive_title">
+							<span>PCHC</span> Departments
+						</h1>
+					</div>
+					<?php $args = array(
+						'orderby' => 'title',
+						'order' => 'ASC',
+						'post_parent' => 0,
+						'post_type' => 'department',
+						'post_status' => 'publish',
+						'posts_per_page' => 999,
+						'nopaging' => true
+					);
+					$wp_query = new WP_Query( $args ); ?>
+					<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+					
+						<?php get_template_part( 'content-archive', get_post_type() ); ?>
+					
+					<?php endwhile; ?>	
+					
+					<?php if (function_exists('page_navi')) { // if expirimental feature is active ?>
+						
+						<?php page_navi(); // use the page navi function ?>
+
+					<?php } else { // if it is disabled, display regular wp prev & next links ?>
+						<nav class="wp-prev-next">
+							<ul class="pager">
+								<li class="previous"><?php next_posts_link(_e('&laquo; Older Entries', "wpbootstrap")) ?></li>
+								<li class="next"><?php previous_posts_link(_e('Newer Entries &raquo;', "wpbootstrap")) ?></li>
+							</ul>
+						</nav>
+					<?php } ?>
+								
+					
+					<?php else : ?>
+					
+					<article id="post-not-found">
+					    <header>
+					    	<h1><?php _e("No Posts Yet", "wpbootstrap"); ?></h1>
+					    </header>
+					    <section class="post_content">
+					    	<p><?php _e("Sorry, What you were looking for is not here.", "wpbootstrap"); ?></p>
+					    </section>
+					    <footer>
+					    </footer>
+					</article>
+					
+					<?php endif; ?>
+			
+				</div> <!-- end #main -->
+
+				<div class="col-sm-6 clearfix" role="complementary">
+					<div id="department-map" data-spy="affix" style="width: 555px;">
+						<?php rewind_posts();
+						if ( have_posts() ) : ?>
+						<div class="acf-map">
+							<?php while ( have_posts() ) : the_post();
+							$address = get_field('address');
+								if( !empty( $address ) ) :?>
+									<div class="marker" data-lat="<?php echo $address['lat']; ?>" data-lng="<?php echo $address['lng']; ?>"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></div>
+								<?php endif; ?>
+							<?php endwhile; ?>
+						</div>
+						<?php endif; ?>
+					</div>
+				</div>
+    
+			</div> <!-- end #content -->
+
+<?php get_footer(); ?>
---
+++ content-archive-department.php
@@ -0,0 +1,42 @@
+<article id="post-<?php the_ID(); ?>" <?php post_class('clearfix'); ?> role="article">
+						
+	<header>
+		
+		<h3 class="h2"><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h3>
+	
+	</header> <!-- end article header -->
+
+	<section class="post_content">
+	
+		<?php the_post_thumbnail( 'wpbs-featured' ); ?>
+	
+		<?php the_excerpt(); ?>
+
+	</section> <!-- end article section -->
+	
+	<footer>
+		<?php $children_args = array(
+			'post_parent' => get_the_ID(),
+			'post_type' => 'department',
+			'post_status' => 'publish',
+			'posts_per_page' => -1,
+		);
+		$children = new WP_Query( $children_args );
+		if( $children->have_posts() ) : while( $children->have_posts() ) : $children->the_post(); ?>
+			<article id="post-<?php the_ID(); ?>" <?php post_class('clearfix'); ?> role="article">
+				<header>
+					<h4><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h4>
+				</header>
+
+				<section class="post_content">
+	
+					<?php the_post_thumbnail( 'wpbs-featured' ); ?>
+				
+					<?php the_excerpt(); ?>
+
+				</section> <!-- end article section -->
+			</article>
+		<?php endwhile; endif; ?>
+	</footer> <!-- end article footer -->
+
+</article> <!-- end article -->

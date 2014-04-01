cfa20945fed270e48b818c71c1b66286722fe21b
Chris Violette <cvioletterun@gmail.com>
2014-04-01 15:53:24 -0400

    Added favicons and Google Analytics

--- functions.php
+++ functions.php
@@ -491,4 +491,20 @@ if( !function_exists( "theme_js" ) ) {
 }
 add_action( 'wp_enqueue_scripts', 'theme_js' );
 
+function google_analytics_tracking_code(){ ?>
+
+    <script>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+      ga('create', 'UA-22274651-4', 'pchc.com');
+      ga('send', 'pageview');
+  </script>
+
+<?php }
+
+add_action('wp_footer', 'google_analytics_tracking_code');
+
 ?>

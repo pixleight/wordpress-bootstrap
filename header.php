69f387ad45d2676983441c612e0afd84981fe465
Chris Violette <cvioletterun@gmail.com>
2014-05-20 12:54:10 -0400

    Removed search from the navbar

--- header.php
+++ header.php
@@ -49,14 +49,6 @@
 
 					<div class="collapse navbar-collapse navbar-responsive-collapse">
 						<?php wp_bootstrap_main_nav(); // Adjust using Menus in Wordpress Admin ?>
-
-						<?php //if(of_get_option('search_bar', '1')) {?>
-						<form class="navbar-form navbar-right" role="search" method="get" id="searchform" action="<?php echo home_url( '/' ); ?>">
-							<div class="form-group">
-								<input name="s" id="s" type="text" class="search-query form-control" autocomplete="off" placeholder="<?php _e('Search','wpbootstrap'); ?>" data-provide="typeahead" data-items="4" data-source='<?php echo $typeahead_data; ?>'>
-							</div>
-						</form>
-						<?php //} ?>
 					</div>
 
 				</div> <!-- end .container -->

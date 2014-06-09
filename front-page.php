961b63ae1b3cc1fae1010a07f25a84f3c7dd3db4
Chris Violette <cvioletterun@gmail.com>
2014-06-09 11:05:07 -0400

    Removed pagination from front page

--- front-page.php
+++ front-page.php
@@ -101,19 +101,6 @@
 						</a>
 					</div>
 					
-					<?php if (function_exists('page_navi')) { // if expirimental feature is active ?>
-						
-						<?php page_navi(); // use the page navi function ?>
-						
-					<?php } else { // if it is disabled, display regular wp prev & next links ?>
-						<nav class="wp-prev-next">
-							<ul class="pager">
-								<li class="previous"><?php next_posts_link(_e('&laquo; Older Entries', "wpbootstrap")) ?></li>
-								<li class="next"><?php previous_posts_link(_e('Newer Entries &raquo;', "wpbootstrap")) ?></li>
-							</ul>
-						</nav>
-					<?php } ?>		
-					
 					<?php else : ?>
 					
 					<article id="post-not-found">

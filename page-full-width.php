baff54e96d77ab631a89ce2ad6c123f70415ca6a
Chris Violette <cvioletterun@gmail.com>
2015-09-16 16:04:10 -0400

    Removed comments template from full-width page.

--- page-full-width.php
+++ page-full-width.php
@@ -5,9 +5,9 @@ Template Name: Full Width Page
 ?>
 
 <?php get_header(); ?>
-			
+
 			<div id="content" class="clearfix row">
-			
+
 				<div id="main" class="col col-lg-12 clearfix" role="main">
 
 					<?php if ( is_active_sidebar( 'announce' ) ) : ?>
@@ -19,34 +19,32 @@ Template Name: Full Width Page
 					<?php endif; ?>
 
 					<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-					
+
 					<article id="post-<?php the_ID(); ?>" <?php post_class('clearfix'); ?> role="article">
-						
+
 						<header>
-							
+
 							<div class="page-header"><h1><?php the_title(); ?></h1></div>
-						
+
 						</header> <!-- end article header -->
-					
+
 						<section class="post_content">
 							<?php the_content(); ?>
-					
+
 						</section> <!-- end article section -->
-						
+
 						<footer>
-			
+
 							<p class="clearfix"><?php the_tags('<span class="tags">' . __("Tags","wpbootstrap") . ': ', ', ', '</span>'); ?></p>
-							
+
 						</footer> <!-- end article footer -->
-					
+
 					</article> <!-- end article -->
-					
-					<?php comments_template(); ?>
-					
-					<?php endwhile; ?>	
-					
+
+					<?php endwhile; ?>
+
 					<?php else : ?>
-					
+
 					<article id="post-not-found">
 					    <header>
 					    	<h1><?php _e("Not Found", "wpbootstrap"); ?></h1>
@@ -57,13 +55,13 @@ Template Name: Full Width Page
 					    <footer>
 					    </footer>
 					</article>
-					
+
 					<?php endif; ?>
-			
+
 				</div> <!-- end #main -->
-    
+
 				<?php //get_sidebar(); // sidebar 1 ?>
-    
+
 			</div> <!-- end #content -->
 
 <?php get_footer(); ?>
